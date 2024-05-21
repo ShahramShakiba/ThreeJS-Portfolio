65f6a82037bc5d37e13208b80b7ac57e644b454d
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-05-21 11:39:46 +0330

    Add a Preloader with a Start Button

--- Portfolio/src/App/UI/Preloader.js
+++ Portfolio/src/App/UI/Preloader.js
@@ -4,13 +4,46 @@ export default class Preloader {
   constructor() {
     this.assetStore = assetStore;
 
+    //===== Access to DOM Elements
+    this.overlay = document.querySelector('.overlay');
+    this.loading = document.querySelector('.loading');
+    this.startButton = document.querySelector('.start');
+
     this.assetStore.subscribe((state) => {
-      console.log(state.loadedAssets);
       this.numberOfLoadedAssets = Object.keys(state.loadedAssets).length;
       this.numberOfAssetsToLoad = state.assetsToLoad.length;
-
       this.progress = this.numberOfLoadedAssets / this.numberOfAssetsToLoad;
+      this.progress = Math.trunc(this.progress * 100); //to omit decimals number
       console.log(this.progress);
+
+      //===== Update progressPercentage in real-time
+      document.getElementById('progressPercentage').innerHTML = this.progress;
+
+      if (this.progress === 100) {
+        this.loading.classList.add('fade');
+        window.setTimeout(() => this.ready(), 1200);
+      }
     });
+  }
+
+  //===== Ready - all assets are being loaded
+  ready() {
+    this.loading.remove();
+    this.startButton.style.display = 'inline';
+    this.startButton.classList.add('fadeIn');
+
+    this.startButton.addEventListener(
+      'click',
+      () => {
+        this.overlay.classList.add('fade');
+        this.startButton.classList.add('fadeOut');
+
+        window.setTimeout(() => {
+          this.overlay.remove();
+          this.startButton.remove();
+        }, 2000);
+      },
+      { once: true }
+    );
   }
 }
