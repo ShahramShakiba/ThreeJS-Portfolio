b4323e2e7d4949ef3e9ffffec98c53165ba4b987
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-05-21 13:43:44 +0330

    Dynamic Object | Fall down a Cube in a Physical World

--- Portfolio/src/App/Camera.js
+++ Portfolio/src/App/Camera.js
@@ -22,7 +22,7 @@ export default class Camera {
       0.1,
       200
     );
-    this.instance.position.z = 5;
+    this.instance.position.z = 100;
   }
   setControls() {
     this.controls = new OrbitControls(this.instance, this.canvas);
--- Portfolio/src/App/World/Physics.js
+++ Portfolio/src/App/World/Physics.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import App from '../App.js';
-import RAPIER from '@dimforge/rapier3d';
 
 export default class Physics {
   constructor() {
@@ -8,10 +7,36 @@ export default class Physics {
     this.scene = this.app.scene;
 
     import('@dimforge/rapier3d').then((RAPIER) => {
+      //===== 01. Setup the Physics World
       const gravity = { x: 0, y: -9.81, z: 0 };
       this.world = new RAPIER.World(gravity);
+
+      //===== 02. Create Mesh
+      const geometry = new THREE.BoxGeometry(1, 1, 1);
+      const material = new THREE.MeshStandardMaterial({ color: 'yellow' });
+      this.cubeMesh = new THREE.Mesh(geometry, material);
+      this.scene.add(this.cubeMesh);
+
+      //===== 03. Create the Rigid Body(represent the Mesh in physical-world)
+      const rigidBodyType = RAPIER.RigidBodyDesc.dynamic();
+      this.rigidBody = this.world.createRigidBody(rigidBodyType);
+
+      const colliderType = RAPIER.ColliderDesc.cuboid(0.5, 0.5, 0.5);
+      this.world.createCollider(colliderType, this.rigidBody);
+
+      //run the loop-method only when rapier has been loaded
+      this.rapierLoaded = true;
     });
   }
 
-  loop() {}
+  loop() {
+    if (!this.rapierLoaded) return;
+    
+    //===== 04. Update Mesh from physics-simulation
+    this.world.step();
+    const position = this.rigidBody.translation();
+    const rotation = this.rigidBody.rotation();
+    this.cubeMesh.position.copy(position);
+    this.cubeMesh.quaternion.copy(rotation);
+  }
 }
