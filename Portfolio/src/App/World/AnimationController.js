899efff8f25c27d493ee0256d1ac5a6c44aacbaa
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-05-24 16:30:38 +0330

    Transitioning Between Animations

--- Portfolio/src/App/World/AnimationController.js
+++ Portfolio/src/App/World/AnimationController.js
@@ -1,11 +1,14 @@
 import * as THREE from 'three';
 import App from '../App';
+import { inputStore } from '../Utils/Store';
 
 export default class AnimationController {
   constructor() {
     this.app = new App();
     this.scene = this.app.scene;
     this.avatar = this.app.world.character.avatar;
+
+    inputStore.subscribe((input) => this.onInput(input));
 
     this.instantiateAnimations();
   }
@@ -19,6 +22,22 @@ export default class AnimationController {
     });
 
     this.animations.get('idle').play();
+  }
+
+  onInput(input) {
+    if (input.forward || input.backward || input.left || input.right) {
+      this.animations.get('run').reset();
+      this.animations.get('run').play();
+      this.animations
+        .get('run')
+        .crossFadeFrom(this.animations.get('idle'), 0.3);
+    } else {
+      this.animations.get('idle').reset();
+      this.animations.get('idle').play();
+      this.animations
+        .get('idle')
+        .crossFadeFrom(this.animations.get('run'), 0.3);
+    }
   }
 
   loop(deltaTime) {
